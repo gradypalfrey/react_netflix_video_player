7f42015ae6309ee1f06f1608b76df6ec8b5f5e70
gpbio10 <40411387+gpbio10@users.noreply.github.com>
2021-04-03 18:55:25 -0400

    major progress

--- src/App.js
+++ src/App.js
@@ -1,234 +1,137 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
 import ReactPlayer from 'react-player';
 import { makeStyles, withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import { Button, Icon } from '@material-ui/core';
-import ArrowBack from '@material-ui/icons/ArrowBack';
+import screenfull from 'screenfull';
 
-import IconButton from '@material-ui/core/IconButton';
-import Replay10Icon from '@material-ui/icons/Replay10';
-import Forward10Icon from '@material-ui/icons/Forward10';
-import PlayArrowIcon from '@material-ui/icons/PlayArrow';
-import PauseIcon from '@material-ui/icons/Pause';
-import Slider from '@material-ui/core/Slider';
-import Tooltip from '@material-ui/core/Tooltip';
-import VolumeUpIcon from '@material-ui/icons/VolumeUp';
-import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
-import SkipNextIcon from '@material-ui/icons/SkipNext';
-import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
-import SubtitlesIcon from '@material-ui/icons/Subtitles';
-import SpeedIcon from '@material-ui/icons/Speed';
-import FullscreenIcon from '@material-ui/icons/Fullscreen';
-
-import Popover from '@material-ui/core/Popover';
-
-import { useState } from 'react';
-import { Button as Button1} from 'reactstrap';
-import { Popover as Popover1 } from 'reactstrap';
-import { PopoverHeader as PopoverHeader1} from 'reactstrap';
-import { PopoverBody as PopoverBody1 } from 'reactstrap';
+import Controls from "./Controls.js";
+import { FormatAlignCenter } from '@material-ui/icons';
 
 
 const useStyles = makeStyles({
-  playerWrapper:{
+  playerWrapper: {
     width: "100%",
     position: "relative",
   },
 
-  controlsWrapper:{
-    position:"absolute",
-    top:0,
-    left:0,
-    right:0,
-    bottom:0,
-    //background:"rgba(0,0,0,0.6)",
-    display:"flex",
-    flexDirection:"column",
-    justifyContent:"space-between",
-    zIndex:1,
-  },
-
-  controlIcons:{
+  controlIcons: {
     color: "lightgrey",
     //width: "15px",
-    "&:hover":{
+    "&:hover": {
       transform: "scale(1.1)",
       color: "white",
     },
   },
 });
 
-function ValueLabelComponent(props) {
-  const { children, open, value } = props;
+const format = (sec) => {
+  if(isNaN(sec)) {
+    return `00:00`;
+  }
+  const date = new Date(sec * 1000);
+  const hours = date.getUTCHours();
+  const minutes = date.getUTCMinutes();
+  const seconds = date.getUTCSeconds().toString().padStart(2,"0");
 
-  return (
-    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
-      {children}
-    </Tooltip>
-  );
-}
+  if (hours) {
+    return `${hours}:${minutes.toString().padStart(2, "0")}:${seconds}`;
+  }
 
-const RedSlider = withStyles({
-  root: {
-    color: 'red',
-  },
-  
-})(Slider);
+  return `${minutes}:${seconds}`;
+};
+
 
 function App() {
   const classes = useStyles();
+  const [state, setState] = useState({
+    playing: true,
+    muted: true,
+    volume: 0.5,
+    playBackRate: 1,
+    played: 0,
+    seeking: false,
+  });
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const { playing, muted, played, seeking } = state;
 
-  const handlePopover = (event) => {
-    setAnchorEl(event.currentTarget);
+  const playerRef = useRef(null);
+  const playerContainerRef = useRef(null);
+
+  const handlePlayPause = () => {
+    setState({ ...state, playing: !state.playing });
   };
 
-  const handleClose = () => {
-    setAnchorEl(null);
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
   };
 
-  const open = Boolean(anchorEl);
-  const id = open ? 'speed-popover' : undefined;
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 10);
+  };
 
+  const toggleFullScreen = () => {
+    screenfull.toggle(playerContainerRef.current);
+  };
+
+  const handleProgress = (changeState) => {
+    if (!state.seeking) {
+      setState({ ...state, ...changeState });
+    }
+  };
+
+  const handleSeekChange = (e, newValue) => {
+    setState({ ...state, played: parseFloat(newValue / 100) });
+  };
+
+  const handleOnSeekMouseDown = (e) => {
+    setState({ ...state, seeking: true });
+  };
+
+  const handleOnSeekMouseUp = (e, newValue) => {
+    setState({ ...state, seeking: false });
+    playerRef.current.seekTo(newValue / 100);
+  };
+
+  const currentTime = playerRef.current ? playerRef.current.getCurrentTime() : '00:00';
+  const duration = playerRef.current ? playerRef.current.getDuration() : '00:00';
+
+  const elapsedTime = format(currentTime);
+  const totalDuration = format(duration);
 
   return (
-    <>
-      <AppBar position="fixed">
-        <Toolbar>
-          <Typography variant="h10">Netflix Video Player</Typography>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-      <Container maxWidth="sm">
-        <div className={classes.playerWrapper}>
+    <div style={{width:"88%", paddingLeft: "6%", backgroundColor: "black"}}>
+      <Container maxWidth="xl" disableGutters={true}>
+        <div ref={playerContainerRef} className={classes.playerWrapper}>
           <ReactPlayer
             width={"100%"}
             height="100%"
             url="https://filesamples.com/samples/video/mp4/sample_1920x1080.mp4"
-            playing={true}
-            muted={false}
+            playing={playing}
+            muted={muted}
+            ref={playerRef}
+            onProgress={handleProgress}
           />
-
-          {/* Back Button*/}
-
-          <div className={classes.controlsWrapper}>
-            <Grid container direction="row" alignItems="center" justify="space-between" style={{padding:16}}>
-              <Grid item>
-                <Button style={{color: "white"}} startIcon={<ArrowBack/>}>
-                </Button>
-              </Grid>
-            </Grid>
-
-            {/* Bottom Controls*/}
-
-
-            <Grid container direction="row" alignItems="center" justify="center">
-
-              <IconButton className={classes.controlIcons} aria-label="reqind">
-                <PlayArrowIcon fontSize="inherit" />
-              </IconButton>
-
-            </Grid>
-
-            <Grid container direction="row" justify="space-between" alignItems="center" style={{padding: 16}}>
-              <Grid item xs={12}>
-                <RedSlider min={0} max={100} defaultValue={20} ValueLabelComponent={ValueLabelComponent} />
-              </Grid>
-
-              <Grid item>
-                <Grid container alignItems="center" direction="row">
-                  <IconButton className={classes.controlIcons}>
-                    <PlayArrowIcon />
-                  </IconButton>
-
-                  <IconButton className={classes.controlIcons}>
-                    <Replay10Icon/>
-                  </IconButton>
-
-                  <IconButton className={classes.controlIcons}>
-                    <Forward10Icon />
-                  </IconButton>
-
-                  <IconButton onClick={handlePopover} className={classes.controlIcons}>
-                    <VolumeUpIcon />
-                  </IconButton>
-                  <Popover
-                  id={id}
-                  open={open}
-                  anchorEl={anchorEl}
-                  onClose={handleClose}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                  }}
-                  transformOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                  }}
-                >
-                  <Slider orientation ="vertical" style={{height: "100px"}} width={100} min={0} max={100}/>
-                </Popover>
-
-                  {/*
-                  <IconButton className={classes.controlIcons}>
-                    <HelpOutlineIcon />
-                  </IconButton>
-
-                  <IconButton className={classes.controlIcons}>
-                    <SkipNextIcon />
-                  </IconButton>
-
-                  <IconButton className={classes.controlIcons}>
-                    <DynamicFeedIcon />
-                  </IconButton>
-
-                  <IconButton className={classes.controlIcons}>
-                    <SubtitlesIcon />
-                  </IconButton>
-
-                  <IconButton className={classes.controlIcons}>
-                    <SpeedIcon />
-                  </IconButton>
-                  */}
-                  
-                </Grid>
-              </Grid>
-              
-              <Grid item>
-                <IconButton className={classes.controlIcons}>
-                  <HelpOutlineIcon />
-                </IconButton>
-
-                <IconButton className={classes.controlIcons}>
-                  <SkipNextIcon />
-                </IconButton>
-
-                <IconButton className={classes.controlIcons}>
-                  <DynamicFeedIcon />
-                </IconButton>
-
-                <IconButton className={classes.controlIcons}>
-                  <SubtitlesIcon />
-                </IconButton>
-
-                <IconButton id="Popover1" type="button" className={classes.controlIcons}>
-                  <SpeedIcon />
-                </IconButton>
-            
-                <IconButton className={classes.controlIcons}>
-                  <FullscreenIcon />
-                </IconButton>
-              </Grid>
-            </Grid>
-          </div>
+          <Controls
+            onPlayPause={handlePlayPause}
+            playing={playing}
+            onRewind={handleRewind}
+            onFastForward={handleFastForward}
+            muted={muted}
+            onToggleFullScreen={toggleFullScreen}
+            played={played}
+            onSeek={handleSeekChange}
+            onSeekMouseDown={handleOnSeekMouseDown}
+            onSeekMouseUp={handleOnSeekMouseUp}
+            elapsedTime={elapsedTime}
+            totalDuration={totalDuration}
+          />
         </div>
       </Container>
-    </>
+    </div>
   );
 }
 
---
+++ src/Controls.js
@@ -0,0 +1,219 @@
+import React from 'react';
+import { duration, makeStyles, withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+import { Button, Icon, Typography } from '@material-ui/core';
+import ArrowBack from '@material-ui/icons/ArrowBack';
+
+import IconButton from '@material-ui/core/IconButton';
+import Replay10Icon from '@material-ui/icons/Replay10';
+import Forward10Icon from '@material-ui/icons/Forward10';
+import PlayArrowIcon from '@material-ui/icons/PlayArrow';
+import PauseIcon from '@material-ui/icons/Pause';
+import Slider from '@material-ui/core/Slider';
+import Tooltip from '@material-ui/core/Tooltip';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
+import HelpOutlineIcon from '@material-ui/icons/HelpOutline';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
+import DynamicFeedIcon from '@material-ui/icons/DynamicFeed';
+import SubtitlesIcon from '@material-ui/icons/Subtitles';
+import SpeedIcon from '@material-ui/icons/Speed';
+import FullscreenIcon from '@material-ui/icons/Fullscreen';
+
+import Popover from '@material-ui/core/Popover';
+
+
+const useStyles = makeStyles({
+
+    controlsWrapper: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        //background:"rgba(0,0,0,0.6)",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        zIndex: 1,
+    },
+
+    controlIcons: {
+        color: "lightgrey",
+        //width: "15px",
+        "&:hover": {
+            transform: "scale(1.1)",
+            color: "white",
+        },
+    },
+});
+
+function ValueLabelComponent(props) {
+    const { children, open, value } = props;
+
+    return (
+        <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+            {children}
+        </Tooltip>
+    );
+}
+
+const RedSlider = withStyles({
+    root: {
+        color: 'red',
+    },
+
+})(Slider);
+
+export default ({onPlayPause, playing, onRewind, onFastForward, onToggleFullScreen, played, onSeek, onSeekMouseDown, onSeekMouseUp, elapsedTime, totalDuration}) => {
+
+    const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopover = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    const open = Boolean(anchorEl);
+    const id = open ? 'speed-popover' : undefined;
+
+    return (
+        <div className={classes.controlsWrapper}>
+            <Grid container direction="row" alignItems="center" style={{ padding: 16 }}>
+                <Grid item>
+                    <Button style={{ color: "white" }} startIcon={<ArrowBack />}>
+                        <Typography>Back to Browse</Typography>
+                    </Button>
+                </Grid>
+            </Grid>
+
+            {/* Bottom Controls*/}
+
+
+            <Grid container direction="row" alignItems="center" justify="center">
+
+                {/* <IconButton className={classes.controlIcons} aria-label="reqind">
+                    <PlayArrowIcon fontSize="inherit" />
+                </IconButton> */}
+
+            </Grid>
+
+            <Grid container direction="row" justify="space-between" alignItems="center" style={{ padding: 16 }}>
+                <Grid item lg={11}>
+                    <RedSlider
+                        min={0}
+                        max={100}
+                        value={played * 100}
+                        ValueLabelComponent={(props) => <ValueLabelComponent {...props} value={elapsedTime}/>}
+                        onChange={onSeek}
+                        onMouseDown={onSeekMouseDown}
+                        onChangeCommitted={onSeekMouseUp}
+                        />
+                </Grid>
+
+                <Grid item>
+                    <Button variant="text" style={{color: "white"}}>
+                        <Typography>{elapsedTime}</Typography>
+                    </Button>
+                </Grid>
+
+                <Grid item>
+                    <Grid container alignItems="center" direction="row">
+                        <IconButton onClick={onPlayPause} className={classes.controlIcons}>
+                            {playing ? (
+                                <PauseIcon />
+                            ) : (
+                                <PlayArrowIcon />
+                            )}
+                        </IconButton>
+
+                        <IconButton onClick={onRewind} className={classes.controlIcons}>
+                            <Replay10Icon />
+                        </IconButton>
+
+                        <IconButton onClick={onFastForward} className={classes.controlIcons}>
+                            <Forward10Icon />
+                        </IconButton>
+
+                        <IconButton onClick={handlePopover} className={classes.controlIcons}>
+                            <VolumeUpIcon />
+                        </IconButton>
+                        <Popover
+                            id={id}
+                            open={open}
+                            anchorEl={anchorEl}
+                            onClose={handleClose}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'center',
+                            }}
+                            transformOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'center',
+                            }}
+                        >
+                            <Slider color="red" orientation="vertical" style={{height: "100px", backgroundColor: "darkgrey", color:"red" }} width={100} min={0} max={100} />
+                        </Popover>
+
+                        <Button variant="text" style={{color: "white"}}>
+                            <Typography>National Geographic S1 E1</Typography>
+                        </Button>
+
+                        {/*
+                <IconButton className={classes.controlIcons}>
+                <HelpOutlineIcon />
+                </IconButton>
+
+                <IconButton className={classes.controlIcons}>
+                <SkipNextIcon />
+                </IconButton>
+
+                <IconButton className={classes.controlIcons}>
+                <DynamicFeedIcon />
+                </IconButton>
+
+                <IconButton className={classes.controlIcons}>
+                <SubtitlesIcon />
+                </IconButton>
+
+                <IconButton className={classes.controlIcons}>
+                <SpeedIcon />
+                </IconButton>
+                */}
+
+                    </Grid>
+                </Grid>
+
+
+                <Grid item>
+                    <IconButton className={classes.controlIcons}>
+                        <HelpOutlineIcon />
+                    </IconButton>
+
+                    <IconButton className={classes.controlIcons}>
+                        <SkipNextIcon />
+                    </IconButton>
+
+                    <IconButton className={classes.controlIcons}>
+                        <DynamicFeedIcon />
+                    </IconButton>
+
+                    <IconButton className={classes.controlIcons}>
+                        <SubtitlesIcon />
+                    </IconButton>
+
+                    <IconButton id="Popover1" type="button" className={classes.controlIcons}>
+                        <SpeedIcon />
+                    </IconButton>
+
+                    <IconButton onClick={onToggleFullScreen} className={classes.controlIcons}>
+                        <FullscreenIcon />
+                    </IconButton>
+                </Grid>
+            </Grid>
+        </div>
+    )
+};
